ebbe42b2f1f74679f2f0586a58df025debd8911c
amora7741 <mora7741@gmail.com>
2024-05-04 15:30:54 -0700

    Resize welcome text

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -33,7 +33,11 @@ const ProjectList = () => {
       repoLink: 'https://github.com/amora7741/MemoryGame',
       liveLink: 'https://memory-game-six-alpha.vercel.app/',
     },
-    { name: 'Coming Soon', description: 'In the works...' },
+    {
+      name: 'Coming Soon',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+    },
   ];
 
   return (
